daaa3735d5707efc0d623bad92cfb63cb6d5ee50
honeybuttercrisp <vietdragon.jv@gmail.com>
2024-09-02 06:03:19 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -7,10 +7,20 @@ public class LoadAssets : MonoBehaviour
 
     public GameObject redObj;
 
+    [SerializeField] private GameObject blueObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Limit framerate to 60fps
+        Application.targetFrameRate = 60;
+
+        // redObj
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+
+        // blueObj
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+
     }
 
     // Update is called once per frame
