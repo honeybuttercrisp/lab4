e46ea5f36211db44d05df7e6c3d000c594f57511
honeybuttercrisp <vietdragon.jv@gmail.com>
2024-09-02 07:18:57 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,12 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int rand;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        rand = Random.Range(150, 251);
+
     }
 
     // Update is called once per frame
@@ -20,5 +21,15 @@ public class PrintAndHide : MonoBehaviour
         i++;
 
         Debug.Log($"{gameObject.name}:{i}");
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.tag == "Blue" && i == rand)
+        {
+            rend.enabled = false;
+        }
+
     }
 }
